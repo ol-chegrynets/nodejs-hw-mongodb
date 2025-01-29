ca13011be8cd2b17fa88e3b03e78a7c158dff961
shura73 <ol.chegrynets@gmail.com>
2025-01-29 21:20:53 +0100

    finish ommmit

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,10 +6,10 @@ export const initMongoConnection = async () => {
     const user = getEnvVar('MONGODB_USER');
     const pwd = getEnvVar('MONGODB_PASSWORD');
     const url = getEnvVar('MONGODB_URL');
-    // const db = getEnvVar('MONGODB_DB');
+    const db = getEnvVar('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/?retryWrites=true&w=majority`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
 
     console.log('====================================');
