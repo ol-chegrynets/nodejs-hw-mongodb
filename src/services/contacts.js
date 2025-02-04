32c6ec8b517e5b200ca470e42d5586b9a7de8f4e
Oleksandr <ol.chegrynets@gmail.com>
2025-02-04 11:32:36 +0100

    Hw3 crud (#7)
    * final commit
    
    * change services

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -26,12 +26,16 @@ export const updateContact = async (contactId, payload, options = {}) => {
     },
   );
 
+<<<<<<< HEAD
+  return rawResult.value;
+=======
   if (!rawResult || !rawResult.value) return null;
 
   return {
     student: rawResult.value,
     isNew: Boolean(rawResult?.lastErrorObject?.upserted),
   };
+>>>>>>> 35bc0a48660bc7261b0f2a119be888431f2d0ece
 };
 
 export const deleteContact = async (contactId) => {
