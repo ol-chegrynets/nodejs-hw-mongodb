2912e3c6320fc397b9908fa778597e163aa26d32
shura73 <ol.chegrynets@gmail.com>
2025-02-04 11:46:25 +0100

    commit

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -1,6 +1,6 @@
-// import 'dotenv/config';
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export const getEnvVar = (envName, defaultPort) => {
   const envVar = process.env[envName];
