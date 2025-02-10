610ca053443ebadf97ac6c2396224cef002ff0ba
Oleksandr <ol.chegrynets@gmail.com>
2025-02-10 19:04:24 +0100

    Hw4 validation (#14)
    * final commit
    
    * error correction
    
    * debag
    
    * +

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -16,7 +16,9 @@ export const getAllContacts = async ({
   if (filter.contactType) {
     contactsQuery.where('contactType').equals(filter.contactType);
   }
-  if (filter.isFavorite) {
+
+  if (filter.isFavourite) {
+
     contactsQuery.where('isFavourite').ne(filter.isFavourite);
   }
 
