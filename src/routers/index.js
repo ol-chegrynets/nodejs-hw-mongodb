7910dc272f5b89733db051dff386b09e78cf0fc4
shura73 <ol.chegrynets@gmail.com>
2025-02-18 14:34:49 +0100

    error message1

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
-import { Router } from 'express';
+import express from 'express';
 import contactsRouter from './contacts.js';
+import authRouter from './auth.js';
 
-const router = Router();
+const router = express.Router();
 
 router.use('/contacts', contactsRouter);
+router.use('/auth', authRouter);
 
 export default router;
