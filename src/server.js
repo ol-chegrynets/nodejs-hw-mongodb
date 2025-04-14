9c821539511e1135c0405d4492e59242540b24e8
Oleksandr <ol.chegrynets@gmail.com>
2025-04-14 15:35:20 +0200

    Hw7-swagger (#25)
    * hw7
    
    * hw7

--- src/server.js
+++ src/server.js
@@ -19,13 +19,13 @@ export const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  // app.use(
+  //   pino({
+  //     transport: {
+  //       target: 'pino-pretty',
+  //     },
+  //   }),
+  // );
 
   app.get('/', (req, res) => {
     res.json({
